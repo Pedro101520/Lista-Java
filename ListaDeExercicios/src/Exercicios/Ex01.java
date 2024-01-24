402b99b6093c1a3658db3a570f92be6d9d40fcc8
Pedro101520 <pedrolimazoi10@gmail.com>
2024-01-24 16:22:48 -0300

    Exercicio 01

---
+++ ListaDeExercicios/src/Exercicios/Ex01.java
@@ -0,0 +1,7 @@
+package Exercicios;
+
+public class Ex01 {
+	public static void main(String args[]) {
+		System.out.println("Olá, Mundo!");
+	}
+}
---
+++ ListaDeExercicios/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module ListaDeExercicios {
+}
