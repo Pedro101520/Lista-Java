1e80533e5df5320c306a1ff1890db18a2b7a2636
Pedro101520 <pedrolimazoi10@gmail.com>
2024-03-22 14:56:32 -0300

    Update Ex59.java

--- ListaDeExercicios/src/Exercicios/Ex59.java
+++ ListaDeExercicios/src/Exercicios/Ex59.java
@@ -7,8 +7,9 @@ public class Ex59 {
 		Scanner inputIdade = new Scanner(System.in);
 		Scanner inputOpcao = new Scanner(System.in);
 		boolean flag = true;
-		int idade = 0, maiorIdade = 0, countHomem = 0, mediaHomem = 0, menorIdade = Integer.MAX_VALUE;
-		char opcao;
+		int idade = 0, maiorIdade = 0, countHomem = 0, menorIdade = Integer.MAX_VALUE;
+		float mediaHomem = 0;
+		char opcao, continuar;
 		
 		while(flag) {
 			System.out.println("Digite sua idade: ");
@@ -22,18 +23,19 @@ public class Ex59 {
 				mediaHomem += idade;
 				countHomem++;
 			}
-			if(opcao == 'f' && menorIdade < idade)
+			if(opcao == 'f' && idade < menorIdade)
 				menorIdade = idade;
 			
-			char continuar;
 			System.out.println("Deseja continuar? S ou N");
-			if(continuar == 's')
+			continuar = inputOpcao.nextLine().charAt(0);
+			if(continuar == 'n')
+				flag = false;
 			
 		}
 		System.out.println("A maior idade lida foi: " + maiorIdade);
 		System.out.printf("Foram cadastrados: %d homens", countHomem);
-		System.out.printf("A mulher mais jovem, tem %d anos", menorIdade);
-		System.out.println("A idade média dos homens é de: " + mediaHomem/countHomem);
+		System.out.printf("\nA mulher mais jovem, tem %d anos", menorIdade);
+		System.out.println("\nA idade média dos homens é de: " + mediaHomem/countHomem);
 
 	}
 }
